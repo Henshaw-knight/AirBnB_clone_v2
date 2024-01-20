46ec7567fa394bee7c0ad6c845f9ecc1e1436594
Henshaw <josephhensh2000@gmail.com>
2024-01-20 02:57:59 -0800

    Write script that prepares MySQL server for the project

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- Script that prepares the MySQL server
+-- creates the test database, user and grant necessary privileges to the user
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
