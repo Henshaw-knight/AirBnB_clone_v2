8c0a6bb764e0f261a2f0ff0868174947c685134c
Henshaw <josephhensh2000@gmail.com>
2024-01-22 03:59:30 -0800

    Update file

--- models/base_model.py
+++ models/base_model.py
@@ -24,14 +24,27 @@ class BaseModel:
             self.updated_at = datetime.now()
 
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            # self.__dict__.update(kwargs)
-            for key, value in kwargs.items():
-                self.key = value
+            if "created_at" in kwargs:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f'
+                                                         )
+            else:
+                self.created_at = datetime.now()
+
+            if "updated_at" in kwargs:
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f'
+                                                         )
+            else:
+                self.updated_at = datetime.now()
+
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+
+            if "__class__" in kwargs:
+                del kwargs['__class__']
+
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
--- models/place.py
+++ models/place.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, Float, String, ForeignKey
+from sqlalchemy import Table, Column, Integer, Float, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.review import Review
 from os import getenv
+
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'), primary_key=True,
+                             nullable=False)
+                      )
 
 
 class Place(BaseModel, Base):
@@ -26,13 +34,16 @@ class Place(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", backref="place",
                                cascade="all, delete-orphan")
-
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates="place_amenities")
     else:
         @property
         def reviews(self):
             """Getter attribute that returns the list of Review instances
             with place_id equals to the current Place.id"""
             from models import storage
+            from models.review import Review
             list_of_reviews = []
             retrieved_reviews = storage.all(Review).values()
 
@@ -40,3 +51,28 @@ class Place(BaseModel, Base):
                 if review.place_id == self.id:
                     list_of_reviews.append(review)
             return list_of_reviews
+
+        @property
+        def amenities(self):
+            """Returns the list of Amenity instances based on the
+            amenity_ids attribute that contains all Amenity.id
+            linked to the Place"""
+            return self.amenity_ids
+
+            # from models import storage
+            # amenities_list = []
+            # retrieved_amenities = storage.all(Amenity).values()
+
+            # for amenity in retrieved_amenities:
+            # if amenity.place_id == self.id:
+            # amenities_list.append(amenity)
+            # return amenities_list
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            """Setter attribute that handles addition of an
+            Amenity.id to the amenity_ids list"""
+            from models.amenity import Amenity
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
+                # self.amenity_ids.append(obj)
