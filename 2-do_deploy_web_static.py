f59c25f30d5bb864510c5b95027f998de2813ffe
Henshaw <josephhensh2000@gmail.com>
2024-02-16 10:45:51 -0800

    Update file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,22 @@ env.user = "ubuntu"
 env.hosts = ["54.144.43.187", "100.26.252.225"]
 
 
+def do_pack():
+    """ Generates a .tgz archive """
+    current_date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = f"web_static_{current_date}.tgz"
+
+    if not os.path.exists("versions"):
+        local("mkdir versions")
+
+    local(f"tar -czvf versions/{archive_name} web_static/")
+    if os.path.exists(archive_name):
+        archive_path = os.path.abspath(archive_name)
+        return archive_path
+    else:
+        return None
+
+
 def do_deploy(archive_path):
     """ Distributes an archive to web servers """
     if not os.path.exists(archive_path):
