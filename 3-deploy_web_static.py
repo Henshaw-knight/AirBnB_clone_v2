f97408708f2b83c9ce114c78ddf86abb8853f8a7
Henshaw <josephhensh2000@gmail.com>
2024-02-17 05:46:56 -0800

    Add files that help with deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-# Fabric script that distributes an archive to the web_servers using do_deploy
-from fabric.api import env, run, put
+"""Fabric script that creates and distributes an archive to the web servers"""
 import os
+from datetime import datetime
+from fabric.api import env, local, run, put
 
 
 env.user = "ubuntu"
@@ -9,7 +10,7 @@ env.hosts = ["54.144.43.187", "100.26.252.225"]
 
 
 def do_pack():
-    """Generates a .tgz archive"""
+    """ Generates a .tgz archive """
     current_date = datetime.now().strftime("%Y%m%d%H%M%S")
     archive_name = f"web_static_{current_date}.tgz"
 
@@ -34,20 +35,26 @@ def do_deploy(archive_path):
         archive_name_with_no_ext = archive_name.split(".")[0]
         path = "/data/web_static/releases/{}/".format(
                 archive_name_with_no_ext)
-
-        # upload archive to the /tmp/ directory of the web server
         put(archive_path, "/tmp/")
-
         run("sudo mkdir -p {}".format(path))
         run("sudo tar -xzvf /tmp/{} -C {}".format(archive_name, path))
         run("sudo rm /tmp/{}".format(archive_name))
         run("sudo mv {}/web_static/* {}".format(path, path))
         run("sudo rm -rf {}web_static".format(path))
         run("sudo rm -rf /data/web_static/current")
-
-        # Create new symbolic link
         run("sudo ln -s {} /data/web_static/current".format(path))
         print("New version deployed!")
         return True
     except Exception:
         return False
+
+
+def deploy():
+    """ Handles full deployment """
+    archive_path = do_pack()
+
+    if archive_path is None:
+        print(archive_path)
+        return False
+    result = do_deploy(archive_path)
+    return result
