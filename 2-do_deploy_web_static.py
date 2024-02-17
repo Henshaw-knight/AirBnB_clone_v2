dd0f40d37f0932f57e36922960ce3a1157bf4fa4
Henshaw <josephhensh2000@gmail.com>
2024-02-16 22:51:44 -0800

    Update file: use format() method

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,22 +8,6 @@ env.user = "ubuntu"
 env.hosts = ["54.144.43.187", "100.26.252.225"]
 
 
-def do_pack():
-    """ Generates a .tgz archive """
-    current_date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_name = f"web_static_{current_date}.tgz"
-
-    if not os.path.exists("versions"):
-        local("mkdir versions")
-
-    local(f"tar -czvf versions/{archive_name} web_static/")
-    if os.path.exists(archive_name):
-        archive_path = os.path.abspath(archive_name)
-        return archive_path
-    else:
-        return None
-
-
 def do_deploy(archive_path):
     """ Distributes an archive to web servers """
     if not os.path.exists(archive_path):
@@ -32,20 +16,21 @@ def do_deploy(archive_path):
     try:
         archive_name = archive_path.split("/")[-1]
         archive_name_with_no_ext = archive_name.split(".")[0]
-        path = f"/data/web_static/releases/{archive_name_with_no_ext}/"
+        path = "/data/web_static/releases/{}/".format(
+                archive_name_with_no_ext)
 
         # upload archive to the /tmp/ directory of the web server
         put(archive_path, "/tmp/")
 
-        run(f"sudo mkdir -p {path}")
-        run(f"sudo tar -xzvf /tmp/{archive_name} -C {path}")
-        run(f"sudo rm /tmp/{archive_name}")
-        run(f"sudo mv {path}/web_static/* {path}")
-        run(f"sudo rm -rf {path}web_static")
+        run("sudo mkdir -p {}".format(path))
+        run("sudo tar -xzvf /tmp/{} -C {}".format(archive_name, path))
+        run("sudo rm /tmp/{}".format(archive_name))
+        run("sudo mv {}/web_static/* {}".format(path, path))
+        run("sudo rm -rf {}web_static".format(path))
         run("sudo rm -rf /data/web_static/current")
 
         # Create new symbolic link
-        run(f"sudo ln -s {path} /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(path))
         print("New version deployed!")
         return True
     except Exception:
